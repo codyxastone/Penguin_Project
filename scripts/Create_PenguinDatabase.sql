86937975f7099a5701db5e616b8be2f4563eb3e0
codyxastone <codyxastone@gmail.com>
2025-11-05 08:22:01 -0500

    Add Create_PenguinDatabase.sql script

---
+++ scripts/Create_PenguinDatabase.sql
@@ -0,0 +1,33 @@
+
+/* Check to see if PenguinDatabase exists, if not it will create the database
+along with the bronze, silver and gold schemas.
+
+WARNING:
+	Rinning this script will drop the entiere 'PenguinDatabase' if it exists.
+	All data in the database will be permanently deleted. Ensure proper backups
+	are up to date before running this script.
+*/
+
+USE master;
+GO
+
+--Drop and recreate the 'PenguinDatabase'
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'PenguinDatabase')
+BEGIN
+	ALTER DATABASE PenguinDatabase SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE PenguinDatabase;
+END;
+GO
+--CREATE 'PenguinDatabase'
+CREATE DATABASE PenguinDatabase;
+GO
+
+USE PenguinDatabase;
+GO
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
